6a217b194def6820770e67502bbe9c0c3d6339d5
yoosion030 <rsy5487@naver.com>
2022-04-18 15:30:46 +0900

    Add baseURL

--- src/Api.js
+++ src/Api.js
@@ -1,3 +1,4 @@
 import axios from "axios";
-axios.defaults.baseURL = "";
+axios.defaults.baseURL =
+  "http://ec2-13-125-40-125.ap-northeast-2.compute.amazonaws.com:8080";
 axios.defaults.withCredentials = true;
